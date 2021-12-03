b15cc268c01c18e87d2e970acdd230a294e7473d
eblair <eblair@raoli.com>
2021-12-03 00:59:23 -0500

    Day 3

--- 2021/AdventOfCode/Days/Day3.swift
+++ 2021/AdventOfCode/Days/Day3.swift
@@ -4,13 +4,96 @@
 //
 
 import Foundation
+import XCTest
 
 final class Day3: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let reports = input.split(separator: "\n")
+        guard let reportLength = reports.first?.count else {
+            return 0
+        }
+        
+        var gammaRate: String = ""
+        var epsilonRate: String = ""
+
+        for idx in 0..<reportLength {
+            let powerConsumption: String = reports.reduce("") { result, report in
+                let char = report[report.index(report.startIndex, offsetBy: idx)]
+                return "\(result)\(char)"
+            }
+            
+            if powerConsumption.count(where: { $0 == "0"}) > powerConsumption.count / 2 {
+                gammaRate.append("0")
+                epsilonRate.append("1")
+            } else {
+                gammaRate.append("1")
+                epsilonRate.append("0")
+            }
+        }
+        
+        guard let gammaRate = Int(gammaRate, radix: 2),
+              let epsilonRate = Int(epsilonRate, radix: 2) else {
+                  return 0
+              }
+        
+        return gammaRate * epsilonRate
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let reports = input.split(separator: "\n")
+        guard let reportLength = reports.first?.count else {
+            return 0
+        }
+        
+        var oxygenRatings = reports
+        var co2Ratings = reports
+
+        for idx in 0..<reportLength {
+            let powerConsumption: String = oxygenRatings.reduce("") { result, report in
+                let char = report[report.index(report.startIndex, offsetBy: idx)]
+                return "\(result)\(char)"
+            }
+            
+            if powerConsumption.count(where: { $0 == "1"}) >= powerConsumption.count(where: { $0 == "0"}) {
+                oxygenRatings = oxygenRatings.filter {
+                    $0[$0.index($0.startIndex, offsetBy: idx)] == "1"
+                }
+            } else {
+                oxygenRatings = oxygenRatings.filter {
+                    $0[$0.index($0.startIndex, offsetBy: idx)] == "0"
+                }
+            }
+            if oxygenRatings.count == 1 {
+                break
+            }
+        }
+        
+        for idx in 0..<reportLength {
+            let powerConsumption: String = co2Ratings.reduce("") { result, report in
+                let char = report[report.index(report.startIndex, offsetBy: idx)]
+                return "\(result)\(char)"
+            }
+            
+            if powerConsumption.count(where: { $0 == "0"}) <= powerConsumption.count(where: { $0 == "1"}) {
+                co2Ratings = co2Ratings.filter {
+                    $0[$0.index($0.startIndex, offsetBy: idx)] == "0"
+                }
+            } else {
+                co2Ratings = co2Ratings.filter {
+                    $0[$0.index($0.startIndex, offsetBy: idx)] == "1"
+                }
+            }
+            
+            if co2Ratings.count == 1 {
+                break
+            }
+        }
+        
+        guard let oxygenRating = Int(oxygenRatings[0], radix: 2),
+              let co2Rating = Int(co2Ratings[0], radix: 2) else {
+                  return 0
+              }
+        
+        return oxygenRating * co2Rating
     }
 }
--- 2021/AdventOfCodeTests/Day3Tests.swift
+++ 2021/AdventOfCodeTests/Day3Tests.swift
@@ -9,11 +9,42 @@ class Day3Tests: XCTestCase {
     let day = Day3()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        let input = """
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        """
+        let result = day.part1(input)
+        XCTAssertEqual(result as? Int, 198)
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        let input = """
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        """
+        let result = day.part2(input)
+        XCTAssertEqual(result as? Int, 230)
+
     }
 
 }
