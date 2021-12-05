956338fb852b6659a1d245d87bce4182d860a702
eblair <eblair@raoli.com>
2021-12-05 02:13:07 -0500

    Refactor Day 5 with Point struct

--- 2021/AdventOfCode/Days/Day5.swift
+++ 2021/AdventOfCode/Days/Day5.swift
@@ -7,59 +7,97 @@ import Foundation
 import RegularExpressionDecoder
 
 final class Day5: Day {
-    typealias Point = (x: Int, y: Int)
+    struct Point {
+        enum Error: Swift.Error {
+            case tooManyValues(Int)
+            case insufficientValues
+        }
+        let x: Int
+        let y: Int
+        
+        init(x: Int, y: Int) {
+            self.x = x
+            self.y = y
+        }
+        
+        init(_ array: [Int]) throws {
+            guard array.count == 2 else {
+                if array.count < 2 {
+                    throw Error.insufficientValues
+                } else {
+                    throw Error.tooManyValues(array.count)
+                }
+            }
+            
+            self.x = array[0]
+            self.y = array[1]
+        }
+
+    }
     
     struct Line: Decodable {
         enum CodingKeys: String, CodingKey {
-            case xStart
-            case yStart
-            case xEnd
-            case yEnd
+            case start
+            case end
         }
 
-        let xStart: Int
-        let yStart: Int
-        let xEnd: Int
-        let yEnd: Int
+        let start: Point
+        let end: Point
         
-        var isHorizontal: Bool { yStart == yEnd }
-        var isVertical: Bool { xStart == xEnd }
-        var maxX: Int { max(xStart, xEnd) }
-        var maxY: Int { max(yStart, yEnd) }
+        var isHorizontal: Bool { start.y == end.y }
+        var isVertical: Bool { start.x == end.x }
+        var maxX: Int { max(start.x, end.x) }
+        var maxY: Int { max(start.y, end.y) }
         
         var normalized: Line {
             if isVertical {
-                return yStart < yEnd ? self : reversed
+                return start.y < end.y ? self : reversed
             } else {
-                return xStart < xEnd ? self : reversed
+                return start.x < end.x ? self : reversed
             }
         }
         
         var reversed: Line {
-            Line(xStart: xEnd, yStart: yEnd, xEnd: xStart, yEnd: yStart)
+            Line(start: end, end: start)
         }
         
         var points: [Point] {
             if isVertical {
-                return (yStart...yEnd).map { (xStart, $0) }
+                return (start.y...end.y).map { Point(x: start.x, y: $0) }
             } else if isHorizontal {
-                return (xStart...xEnd).map { ($0, yStart) }
+                return (start.x...end.x).map { Point(x: $0, y: start.y) }
             } else {
-                let yStride = yStart < yEnd ? 1 : -1
-                return (xStart...xEnd).map { x in
-                    (x, yStart + (x - xStart) * yStride)
+                let yStride = start.y < end.y ? 1 : -1
+                return (start.x...end.x).map { x in
+                    Point(x: x, y: start.y + (x - start.x) * yStride)
                 }
             }
+        }
+        
+        init(start: Point, end: Point) {
+            self.start = start
+            self.end = end
+        }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let start: [Int] = try container.decode(String.self, forKey: .start)
+                .components(separatedBy: ",")
+                .compactMap(Int.init)
+            let end = try container.decode(String.self, forKey: .end)
+                .components(separatedBy: ",")
+                .compactMap(Int.init)
+
+            self.start = try Day5.Point(start)
+            self.end = try Day5.Point(end)
         }
     }
     
     let pattern: RegularExpressionPattern<Line, Line.CodingKeys> = #"""
     \b
-    (?<\#(.xStart)>\d+),
-    (?<\#(.yStart)>\d+)
+    (?<\#(.start)>\d+,\d+)
     \s->\s
-    (?<\#(.xEnd)>\d+),
-    (?<\#(.yEnd)>\d+)
+    (?<\#(.end)>\d+,\d+)
     \b
     """#
     
