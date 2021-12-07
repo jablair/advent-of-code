a9a5a8c7d2f467bb8220e35fed22b14c2b9ecf78
eblair <eblair@raoli.com>
2021-12-07 00:50:34 -0500

    Compound runner

--- 2021/AdventOfCode/Days/Day.swift
+++ 2021/AdventOfCode/Days/Day.swift
@@ -11,3 +11,12 @@ protocol Day: AnyObject {
     func part1(_ input: String) throws -> CustomStringConvertible
     func part2(_ input: String) throws -> CustomStringConvertible
 }
+
+extension Day {
+    func run(_ input: String) throws -> (CustomStringConvertible, CustomStringConvertible) {
+        let p1 = try part1(input)
+        let p2 = try part2(input)
+        
+        return (p1, p2)
+    }
+}
