50affedc53dcf9deee3fdc185af9079bbc165d40
eblair <eblair@raoli.com>
2021-12-12 14:07:41 -0500

    Day 12

--- 2021/AdventOfCode/Days/Day12.swift
+++ 2021/AdventOfCode/Days/Day12.swift
@@ -7,9 +7,68 @@ import Foundation
 
 final class Day12: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let pathDefinitions = input
+            .components(separatedBy: "\n")
+            .map { $0.components(separatedBy: "-") }
+        
+        var paths: [String: [String]] = [:]
+        for def in pathDefinitions {
+            paths[def[0], default: []].append(def[1])
+            paths[def[1], default: []].append(def[0])
+        }
+        
+        var vertices: Set<String> = []
+        pathDefinitions.forEach { path in
+            vertices.insert(path[0])
+            vertices.insert(path[1])
+        }
+        
+        let outputPaths = findCavePaths(paths: paths)
+                
+        return outputPaths.count
     }
+    
+    func findCavePaths(paths: [String: [String]]) -> [[String]] {
+        let smallCaveRemaining: [String: Int] = paths.keys.reduce(into: [:]) { partialResult, key in
+            guard key.first?.isLowercase == true else {
+                return
+            }
+            
+            partialResult[key] = 1
+        }
+        
+        return findPaths(from: "start", in: paths, smallCaveRemaining: smallCaveRemaining)
+    }
+    
+    func findPaths(from start: String, in paths: [String: [String]], smallCaveRemaining: [String: Int]) -> [[String]] {
+        var smallCaveRemaining = smallCaveRemaining
 
+        let destinations = paths[start, default: []]
+            .filter {
+                guard $0.first?.isLowercase == true else {
+                    return true
+                }
+                return smallCaveRemaining[$0] != 0
+            }
+        if start.first?.isLowercase == true {
+            smallCaveRemaining[start, default: 1] -= 1
+        }
+        
+        var outputPaths: [[String]] = []
+        for destination in destinations {
+            if destination == "end" {
+                outputPaths.append([start, "end"])
+            } else {
+                let subpaths = findPaths(from: destination, in: paths, smallCaveRemaining: smallCaveRemaining)
+                for subpath in subpaths {
+                    outputPaths.append([start] + subpath)
+                }
+            }
+        }
+        
+        return outputPaths
+    }
+    
     func part2(_ input: String) -> CustomStringConvertible {
         return 0
     }
--- 2021/AdventOfCodeTests/Day12Tests.swift
+++ 2021/AdventOfCodeTests/Day12Tests.swift
@@ -7,13 +7,66 @@ import XCTest
 
 class Day12Tests: XCTestCase {
     let day = Day12()
+    var input = """
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+    """
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        let result = day.part1(input)
+        XCTAssertEqual(result as? Int, 10)
+    }
+    
+    func testPart1Medium() throws {
+        let input = """
+        dc-end
+        HN-start
+        start-kj
+        dc-start
+        dc-HN
+        LN-dc
+        HN-end
+        kj-sa
+        kj-HN
+        kj-dc
+        """
+        let result = day.part1(input)
+        XCTAssertEqual(result as? Int, 19)
+    }
+    
+    func testPart1Large() throws {
+        let input = """
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
+        """
+        let result = day.part1(input)
+        XCTAssertEqual(result as? Int, 226)
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        let result = day.part2(input)
+        XCTAssertEqual(result as? Int, 0)
     }
 
 }
