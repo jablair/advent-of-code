319b2fae7bf40dcaabfb59fc77ee8e009cd5cf20
eblair <eblair@raoli.com>
2022-12-03 01:20:22 -0500

    Day 3

--- 2022/AdventOfCode/Days/Day3.swift
+++ 2022/AdventOfCode/Days/Day3.swift
@@ -4,13 +4,39 @@
 //
 
 import Foundation
+import Algorithms
 
 final class Day3: Day {
     func part1(_ input: String) async throws -> Int {
-        return 0
+        return input
+            .lines
+            .map {
+                let mid = $0.index($0.startIndex, offsetBy: $0.count / 2)
+                return (Set($0[..<mid]), Set($0[mid...]))
+            }.compactMap {
+                $0.0.intersection($0.1).first.map(Character.init)
+            }.map { value(of: $0) }
+            .sum()
     }
 
     func part2(_ input: String) async throws -> Int {
-        return 0
+        return input
+            .lines
+            .map(Set.init)
+            .chunks(ofCount: 3)
+            .compactMap {
+                $0[$0.startIndex]
+                    .intersection($0[$0.index($0.startIndex, offsetBy: 1)])
+                    .intersection($0[$0.index($0.startIndex, offsetBy: 2)])
+                    .first
+            }.map { value(of: $0) }
+            .sum()
     }
+    
+    private func value(of char: Character) -> Int {
+        guard let dupeVal = char.lowercased().first!.asciiValue else { return 0 }
+        let value = Int(dupeVal - ("a" as Character).asciiValue! + 1)
+        return char.isLowercase ? value : value + 26
+    }
+    
 }
--- 2022/AdventOfCodeTests/Day3Tests.swift
+++ 2022/AdventOfCodeTests/Day3Tests.swift
@@ -7,15 +7,27 @@ import XCTest
 
 class Day3Tests: XCTestCase {
     let day = Day3()
+    let input = """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """
+
+    override func setUp() async throws {
+        day.setup(input)
+    }
 
     func testPart1() async throws {
-        let result = try await day.part1("")
-        debugPrint(result)
+        let result = try await day.part1(input)
+        XCTAssertEqual(result, 157)
     }
 
     func testPart2() async throws {
-        let result = try await day.part2("")
-        debugPrint(result)
+        let result = try await day.part2(input)
+        XCTAssertEqual(result, 70)
     }
 
 }
