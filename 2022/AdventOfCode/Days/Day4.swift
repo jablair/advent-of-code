b7158134796fc3b8d74dddb97305bd0407bb360c
eblair <eblair@raoli.com>
2022-12-04 00:45:51 -0500

    Day 4

--- 2022/AdventOfCode/Days/Day4.swift
+++ 2022/AdventOfCode/Days/Day4.swift
@@ -4,13 +4,59 @@
 //
 
 import Foundation
+import RegularExpressionDecoder
 
 final class Day4: Day {
+    struct Assignments: Decodable {
+        enum CodingKeys: String, CodingKey {
+            case leftMin
+            case leftMax
+            case rightMin
+            case rightMax
+        }
+        
+        let left: Set<Int>
+        let right: Set<Int>
+        
+        var isFullyOverlapping: Bool {
+            left.isSubset(of: right) || right.isSubset(of: left)
+        }
+        
+        var isOverlapping: Bool {
+            !left.intersection(right).isEmpty
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let leftMin = try container.decode(Int.self, forKey: .leftMin)
+            let leftMax = try container.decode(Int.self, forKey: .leftMax)
+            let rightMin = try container.decode(Int.self, forKey: .rightMin)
+            let rightMax = try container.decode(Int.self, forKey: .rightMax)
+            
+            left = Set(leftMin...leftMax)
+            right = Set(rightMin...rightMax)
+        }
+    }
+    
+    var assignments: [Assignments]!
+
+    func setup(_ input: String) throws {
+        let pattern: RegularExpressionPattern<Assignments, Assignments.CodingKeys> = #"""
+            \b
+            (?<\#(.leftMin)>\d+)-(?<\#(.leftMax)>\d+),
+            (?<\#(.rightMin)>\d+)-(?<\#(.rightMax)>\d+)
+            \b
+        """#
+
+        let decoder = try RegularExpressionDecoder<Assignments>(pattern: pattern, options: .allowCommentsAndWhitespace)
+        assignments = try decoder.decode([Assignments].self, from: input)
+    }
+    
     func part1(_ input: String) async throws -> Int {
-        return 0
+        return assignments.filter(\.isFullyOverlapping).count
     }
 
     func part2(_ input: String) async throws -> Int {
-        return 0
+        return assignments.filter(\.isOverlapping).count
     }
 }
--- 2022/AdventOfCodeTests/Day4Tests.swift
+++ 2022/AdventOfCodeTests/Day4Tests.swift
@@ -7,15 +7,27 @@ import XCTest
 
 class Day4Tests: XCTestCase {
     let day = Day4()
-
+    let input = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    """
+    
+    override func setUp() async throws {
+        try day.setup(input)
+    }
+    
     func testPart1() async throws {
-        let result = try await day.part1("")
-        debugPrint(result)
+        let result = try await day.part1(input)
+        XCTAssertEqual(result, 2)
     }
 
     func testPart2() async throws {
-        let result = try await day.part2("")
-        debugPrint(result)
+        let result = try await day.part2(input)
+        XCTAssertEqual(result, 4)
     }
 
 }
