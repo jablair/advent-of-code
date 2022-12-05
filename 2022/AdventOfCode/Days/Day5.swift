5e17a9c5c2e6b0e4b3370d0ac3ee6c0ad6cb386f
eblair <eblair@raoli.com>
2022-12-05 01:16:34 -0500

    Day 5

--- 2022/AdventOfCode/Days/Day5.swift
+++ 2022/AdventOfCode/Days/Day5.swift
@@ -4,13 +4,79 @@
 //
 
 import Foundation
+import Parsing
+import DequeModule
 
 final class Day5: Day {
-    func part1(_ input: String) async throws -> Int {
-        return 0
+    
+    struct Move {
+        let count: Int
+        let sourcec: Int
+        let destination: Int
     }
 
-    func part2(_ input: String) async throws -> Int {
-        return 0
+    var stacks: [[Character]]! = []
+    var moves: [Move]!
+
+    func setup(_ input: String) throws {
+        let segments = input.lines.split(whereSeparator: \.isEmpty)
+        
+        let moveParser = Parse {
+            "move "
+            Int.parser()
+            " from "
+            Int.parser()
+            " to "
+            Int.parser()
+        }.map { Move(count: $0, sourcec: $1, destination: $2)}
+        
+        moves = try segments[1].map {
+            try moveParser.parse($0)
+        }
+        
+        let stackLines = segments[0]
+            .map { $0.chunks(ofCount: 4) }
+            .reversed()
+            .dropFirst(1)
+        
+        let count = stackLines.first!.count
+        stacks = Array(repeating: [], count: count)
+        
+        stackLines.forEach {
+            $0.enumerated().forEach {
+                let (index, element) = $0
+                let crateIDIndex = element.index(element.startIndex, offsetBy: 1)
+                let crate = element[crateIDIndex]
+                if crate.isLetter {
+                    stacks[index].append(crate)
+                }
+            }
+        }
+    }
+    
+    func part1(_ input: String) async throws -> String {
+        var mutatedStacks = stacks!
+        
+        moves.forEach { move in
+            for _ in 0..<move.count {
+                mutatedStacks[move.destination - 1].append(mutatedStacks[move.sourcec - 1].removeLast())
+            }
+        }
+        
+        return String(mutatedStacks.compactMap { $0.last })
+    }
+
+    func part2(_ input: String) async throws -> String {
+        var mutatedStacks = stacks!
+        
+        moves.forEach { move in
+            var moving: Deque<Character> = []
+            for _ in 0..<move.count {
+                moving.insert(mutatedStacks[move.sourcec - 1].removeLast(), at: 0)
+            }
+            mutatedStacks[move.destination - 1].append(contentsOf: moving)
+        }
+        
+        return String(mutatedStacks.compactMap { $0.last })
     }
 }
--- 2022/AdventOfCodeTests/Day5Tests.swift
+++ 2022/AdventOfCodeTests/Day5Tests.swift
@@ -7,15 +7,29 @@ import XCTest
 
 class Day5Tests: XCTestCase {
     let day = Day5()
+    let input = [
+    "    [D]    ",
+    "[N] [C]    ",
+    "[Z] [M] [P]",
+    " 1   2   3 ",
+    "",
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2"
+    ].joined(separator: "\n")
 
+    override func setUp() async throws {
+        try day.setup(input)
+    }
     func testPart1() async throws {
-        let result = try await day.part1("")
-        debugPrint(result)
+        let result = try await day.part1(input)
+        XCTAssertEqual(result, "CMZ")
     }
 
     func testPart2() async throws {
-        let result = try await day.part2("")
-        debugPrint(result)
+        let result = try await day.part2(input)
+        XCTAssertEqual(result, "MCD")
     }
 
 }
