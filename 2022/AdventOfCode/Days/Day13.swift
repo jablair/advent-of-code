9074171bafd76f4c75694060c35198a8849bba17
eblair <eblair@raoli.com>
2022-12-13 21:05:14 -0500

    Day 13

--- 2022/AdventOfCode/Days/Day13.swift
+++ 2022/AdventOfCode/Days/Day13.swift
@@ -4,13 +4,133 @@
 //
 
 import Foundation
+import Parsing
 
 final class Day13: Day {
+    
+    enum MessageContent: Equatable {
+        case integer(Int)
+        indirect case list([MessageContent])
+    }
+    
+    private func parse(list: inout ArraySlice<Character>) -> [MessageContent] {
+        var content: [MessageContent] = []
+        
+        var currentIntString = ""
+        
+    listLoop:
+        while let first = list.first {
+            list = list.dropFirst()
+            switch first {
+            case _ where first.isNumber:
+                currentIntString.append(first)
+            case "[":
+                if !currentIntString.isEmpty, let int = Int(currentIntString) {
+                    content.append(.integer(int))
+                    currentIntString = ""
+                }
+                content.append(.list(parse(list: &list)))
+                
+            case "]":
+                if !currentIntString.isEmpty, let int = Int(currentIntString) {
+                    content.append(.integer(int))
+                    currentIntString = ""
+                }
+                break listLoop
+            default:
+                if !currentIntString.isEmpty, let int = Int(currentIntString) {
+                    content.append(.integer(int))
+                    currentIntString = ""
+                }
+            }
+        }
+
+        return content
+    }
+    
+    func validate(list left: [Day13.MessageContent], right: [Day13.MessageContent]) -> ComparisonResult {
+        for idx in 0..<min(left.count, right.count) {
+            let leftItem = left[idx]
+            let rightItem = right[idx]
+            
+            switch (leftItem, rightItem) {
+            case let (.integer(leftInt), .integer(rightInt)):
+                if leftInt < rightInt {
+                    return .orderedDescending
+                } else if leftInt > rightInt {
+                    return .orderedAscending
+                }
+            case let (.list(leftList), .list(rightList)):
+                let listOrder = validate(list: leftList, right: rightList)
+                if listOrder != .orderedSame {
+                    return listOrder
+                }
+            case let (.integer(leftInt), .list(rightList)):
+                let leftList = [MessageContent.integer(leftInt)]
+                let listOrder = validate(list: leftList, right: rightList)
+                if listOrder != .orderedSame {
+                    return listOrder
+                }
+            case let (.list(leftList), .integer(rightInt)):
+                let rightList = [MessageContent.integer(rightInt)]
+                let listOrder = validate(list: leftList, right: rightList)
+                if listOrder != .orderedSame {
+                    return listOrder
+                }
+            }
+        }
+        
+        if left.count > right.count {
+            return .orderedAscending
+        } else if left.count < right.count {
+            return .orderedDescending
+        }
+        
+        return .orderedSame
+    }
+    
     func part1(_ input: String) async throws -> Int {
-        return 0
+        let validPairs = input
+            .lines
+            .split(whereSeparator: \.isEmpty)
+            .map {
+                var content1 = Array($0.first!).dropFirst()
+                var content2 = Array($0.last!).dropFirst()
+                let m1 = parse(list: &content1)
+                let m2 = parse(list: &content2)
+                return (m1, m2)
+            }.enumerated()
+            .filter { messageInfo in
+                let id = messageInfo.0 + 1
+                let messages = messageInfo.1
+                return validate(list: messages.0, right: messages.1) == .orderedDescending
+            }
+        
+        return validPairs.map { $0.0 + 1 }.sum()
     }
 
     func part2(_ input: String) async throws -> Int {
-        return 0
+        var messages = input
+            .lines
+            .filter { !$0.isEmpty }
+            .map {
+                var content = Array($0).dropFirst()
+                return parse(list: &content)
+            }
+
+        let divider1: [MessageContent] = [.list([.integer(2)])]
+        let divider2: [MessageContent] = [.list([.integer(6)])]
+        
+        messages.append(divider1)
+        messages.append(divider2)
+        
+        messages = messages.sorted { left, right in
+            validate(list: left, right: right) == .orderedDescending
+        }
+        
+        let divider1Idx = messages.firstIndex(of: divider1)! + 1
+        let divider2Idx = messages.firstIndex(of: divider2)! + 1
+        
+        return divider1Idx * divider2Idx
     }
 }
--- 2022/AdventOfCodeTests/Day13Tests.swift
+++ 2022/AdventOfCodeTests/Day13Tests.swift
@@ -8,7 +8,29 @@ import XCTest
 class Day13Tests: XCTestCase {
     let day = Day13()
     let input = """
-    
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
     """
 
     override func setUp() async throws {
@@ -17,12 +39,12 @@ class Day13Tests: XCTestCase {
     
     func testPart1() async throws {
         let result = try await day.part1(input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 13)
     }
 
     func testPart2() async throws {
         let result = try await day.part2(input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 140)
     }
 
 }
